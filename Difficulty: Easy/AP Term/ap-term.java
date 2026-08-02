9a0ca632404e59828765d58c4df941b38ee6116c
khushipandya01 <khushipandya0112@gmail.com>
2026-08-02 23:37:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/AP Term/ap-term.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+
+        int nthTerm = a + (n - 1) * d;
+
+        System.out.println(nthTerm);
+
+        sc.close();
+    }
+}
